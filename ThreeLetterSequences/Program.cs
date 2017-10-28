53f24e70a263e9955bfb52aca437737779d1fe62
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2017-10-29 00:03:09 +0100

    first version using if statement

--- ThreeLetterSequences/Program.cs
+++ ThreeLetterSequences/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ThreeLetterSequences
 {
@@ -10,6 +11,17 @@ namespace ThreeLetterSequences
     {
         static void Main(string[] args)
         {
+            string input = File.ReadAllText("SampleText.txt");
+            int counter = 0;
+
+            for (int i = 0; i <  input.Length; i++)
+            {
+                if (input[i] == 't' && input[i + 1] == 'r' && input[i + 2] == 'a')
+                    counter++;
+            }
+
+            Console.WriteLine(counter);
+            Console.ReadLine();
         }
     }
 }
