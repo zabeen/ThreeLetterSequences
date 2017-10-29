8435acd42631f8846de71db60b5f69fde0801fb8
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2017-10-29 17:30:26 +0000

    non-alphabetical characters are ignored

--- ThreeLetterSequences/Program.cs
+++ ThreeLetterSequences/Program.cs
@@ -11,34 +11,39 @@ namespace ThreeLetterSequences
     class Program
     {
         static Dictionary<string, int> _dict = new Dictionary<string, int>();
-        static string _input = "";
+        static string _fileContents = "";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Reading file...");
-            ReadInFile("SampleText.txt");
+            ImportFileContentsAsString("SampleText.txt");
 
             Console.WriteLine("Creating dictionary...");
             CreateDictionary(3);
 
             int val = GetTLSCount();
-
             Console.WriteLine($"\nTLSs with count {val}:\n" + string.Join(", ", _dict.Where(d => d.Value == val).Select(d => d.Key).ToList()));
+
             Console.WriteLine("\n10 most common TLSs:\n" + string.Join("\n", _dict.OrderByDescending(d => d.Value).Take(10).Select(d => $"{d.Key} - {d.Value}").ToList()));
 
             Console.ReadLine();
         }
 
-        static void ReadInFile(string filePath)
+        static void ImportFileContentsAsString(string filePath)
         {
-            _input = File.ReadAllText(filePath);
+            // read in file and convert to lowercase
+            _fileContents = File.ReadAllText(filePath).ToLower();
+
+            // remove all non-alphabet chars from string
+            Regex regex = new Regex("[^a-z]");
+            _fileContents = regex.Replace(_fileContents, "");
         }
 
         static void CreateDictionary(int tlsLength, string str = "")
         {
             if (str.Length == tlsLength)
             {
-                _dict.Add(str, new Regex(str, RegexOptions.IgnoreCase).Matches(_input).Count);
+                _dict.Add(str, new Regex(str, RegexOptions.IgnoreCase).Matches(_fileContents).Count);
                 return;
             }
             else
